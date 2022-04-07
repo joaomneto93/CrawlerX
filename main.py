3b3cb4355f49299546d82bda0c84330dfe703b20
Joao Cerqueira <joaomneto93@gmail.com>
2022-04-06 22:55:37 -0300

    adição do driver do google chrome e funções iniciais

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-import time
 from selenium import webdriver
-from selenium.webdriver import Chrome
+# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.service import Service
+import time
 
 
-def get_driver(url):
+def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("disable-infobars")
     options.add_argument("start-maximized")
     options.add_argument('disable-dev-shm-usage')
     options.add_argument("no-sandbox")
 
-    driver = Chrome(service=Service("chromedriver_win32/chromedriver.exe"), options=options)
-    driver.get(url=url)
+    driver = webdriver.Chrome(service=Service("chromedriver_win32/chromedriver.exe"), options=options)
+    driver.get("https://www.drogaraia.com.br/")
     return driver
 
-class DrogaRaia:
-    def __init__(self):
-        self.driver = None
-        self.url = "https://www.drogaraia.com.br/"
+
+def main():
+    driver = get_driver()
+    time.sleep(10)
+    element = driver.find_element(by=By.XPATH, value="/html/body/div[13]/div/div[2]/div/div[3]/div[6]/div[3]/div[2]/div[2]")
+    return element
+
+
+if __name__ == '__main__':
+    print('Static text running')
+    print(main())
